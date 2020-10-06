d9e5ff8dfec84c5774c021213dfe8d5c7fe2a7ef
carlo <mi0772@gmail.com>
2020-10-06 09:54:24 +0200

    property name in english and docker images renamed

--- docker-compose/build-all.sh
+++ docker-compose/build-all.sh
@@ -1,12 +1,12 @@
 cd ../logger
 mvn clean install -DskipTests
-docker build -t istat/logger .
+docker build -t mecdcme/is2-logger .
 cd ../dataset
 mvn clean install -DskipTests
-docker build -t istat/dataset .
+docker build -t mecdcme/is2-dataset .
 cd ../workflow-monitor
 mvn clean install -DskipTests
-docker build -t istat/workflow-monitor .
+docker build -t mecdcme/is2-workflow-monitor .
 cd ../notificator
 mvn clean install -DskipTests
-docker build -t istat/notificator .
+docker build -t mecdcme/is2-notificator .
