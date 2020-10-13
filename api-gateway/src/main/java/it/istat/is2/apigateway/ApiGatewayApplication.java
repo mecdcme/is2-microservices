07fe4066879e48dd41795a560757b530d4294d7f
carlo <mi0772@gmail.com>
2020-10-13 15:39:39 +0200

    added jwt security to api-gateway

--- api-gateway/src/main/java/it/istat/is2/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/it/istat/is2/apigateway/ApiGatewayApplication.java
@@ -1,9 +1,13 @@
 package it.istat.is2.apigateway;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -13,5 +17,6 @@ public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
+
 
 }
