306656e13afb092f0099f299c81fbc8a650da211
Francesco Amato <francamato@gmail.com>
2020-11-18 19:05:40 +0100

    adding design services

--- is2-design/src/main/java/it/istat/is2/design/controller/GSBPMProcessController.java
+++ is2-design/src/main/java/it/istat/is2/design/controller/GSBPMProcessController.java
@@ -4,20 +4,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import it.istat.is2.commons.dto.design.domain.BusinessProcessDTO;
 import it.istat.is2.commons.dto.design.domain.GsbpmProcessDTO;
-import it.istat.is2.commons.dto.design.domain.ProcessStepDTO;
-import it.istat.is2.commons.request.business.BusinessProcessRequestForm;
-import it.istat.is2.design.service.BusinessStepService;
 import it.istat.is2.design.service.GsbpmProcessService;
 import it.istat.is2.design.translators.Translators;
 import lombok.extern.slf4j.Slf4j;
@@ -30,7 +22,6 @@ public class GSBPMProcessController {
 	@Autowired
 	private GsbpmProcessService gsbpmProcessService;
 
-	
 	@GetMapping
 	public List<GsbpmProcessDTO> findAllProcesses() {
 
@@ -38,14 +29,13 @@ public class GSBPMProcessController {
 				.collect(Collectors.toList());
 
 	}
-	
+
 	@GetMapping("/{id}")
 	public GsbpmProcessDTO getProcess(@PathVariable("id") Long id) {
 
-		return Translators.translate(gsbpmProcessService.findById(id) );
+		return Translators.translate(gsbpmProcessService.findById(id));
 
 	}
-	
 
 	@GetMapping("/{id}/sub-processes")
 	public List<GsbpmProcessDTO> getSubProccess(@PathVariable("id") Long idParent) {
@@ -54,7 +44,7 @@ public class GSBPMProcessController {
 				.map(x -> Translators.translate(x)).collect(Collectors.toList());
 
 	}
-	
+
 	@GetMapping("/sub-processes")
 	public List<GsbpmProcessDTO> findAllSubProcesses() {
 
