12aecec1e5ff9bbfcf518cc447fbf54df1d31669
carlo <mi0772@gmail.com>
2020-11-19 14:12:14 +0100

    pool hikari

--- docker-compose/build-all.sh
+++ docker-compose/build-all.sh
@@ -1,15 +1,15 @@
-cd ../logger
+cd ../is2-logger
 mvn clean install -DskipTests
 docker build -t mecdcme/is2-logger .
-cd ../dataset
+cd ../is2-dataset
 mvn clean install -DskipTests
 docker build -t mecdcme/is2-dataset .
-cd ../workflow-monitor
+cd ../is2-workflow-monitor
 mvn clean install -DskipTests
 docker build -t mecdcme/is2-workflow-monitor .
-cd ../notificator
+cd ../is2-notificator
 mvn clean install -DskipTests
 docker build -t mecdcme/is2-notificator .
-cd ../api-gateway
+cd ../is2-api-gateway
 mvn clean install -DskipTests
 docker build -t mecdcme/is2-api-gateway .
